3c114e79a4b9ea22ebf6c453149c6adaa1a77c28
Shaf8808 <shadmankhan08@gmail.com>
2023-02-09 14:25:14 +0000

    Set up structure of game functions in JS

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+function runGame() {
+
+
+}
+
+function checkAnswer() {
+
+
+
+}
+
+function calculateCorrectAnswer() {
+
+
+
+}
+
+function incrementScore() {
+
+
+    
+}
+
+function incrementWrongAnswer() {
+
+
+    
+}
+
+function displayAdditionQuestion() {
+
+
+    
+}
+
+function displaySubtractQuestion() {
+
+
+    
+}
+
+function displayMultiplyQuestion() {
+
+
+    
+}
+
+
+
+
+
+
